daa2fd48b999782b4ba9fe10ddfd979ddcf241ae
Angga Pradikta <AnggaPradiktas@users.noreply.github.com>
2022-01-02 11:42:46 +0700

    Update hackernews.py

--- dags/sql/hackernews.py
+++ dags/sql/hackernews.py
@@ -1,7 +1,14 @@
 SQL_QUERY = {
         "HACKERNEWS_COMMENT": """
-                            SELECT *
-                            FROM `bigquery-public-data.hacker_news.comments`
-                            LIMIT 10;
-                """                
+                SELECT *
+                FROM `bigquery-public-data.hacker_news.comments`
+                LIMIT 10;
+        """,
+        
+        "HACKERNEWS_STORIES": """
+                SELECT *
+                FROM `bigquery-public-data.hacker_news.stories`
+                WHERE url IS NOT NULL
+                LIMIT 10;
+        """
 }
